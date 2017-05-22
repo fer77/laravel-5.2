e7b76562999cfed9692aa45e3c0314566e78e679
Fernando Fernandez <ffmp777@yahoo.com>
2017-05-22 09:33:19 -0600

    updated routes to use a validation controller

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,26 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::post('/', function(Request $request) {
+// 	// dd($request->all());
+// 	$validator = Validator::make($request->all(), [
+// 		'email.*' => 'required|email'
+// 	], [
+// 		'email.*' => 'This address must be formatted properly.'
+// 	]);
+// 	if ($validator->fails()) {
+// 		return back()->withInput()->withErrors($validator);
+// 	}
+// 	return 'Validation was a success!!';
+// });
+// All the above can be replaced with this:
+Route::post('/', 'InvitationsController@store');
 
 Auth::routes(); 
 
